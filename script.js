32c039711b67a25cbd7c80498673cc8e8eadc7ca
ajessiah <jessyahanderson@gmail.com>
2025-07-12 01:27:33 -0400

    Home MQ: Desktop

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const toHome = () => {
         });
 };
 
-// window.onload = toHome;
+window.onload = toHome;
 homeBtn.addEventListener("click", toHome);
 logoDiv.addEventListener("click", toHome);
 
@@ -86,6 +86,5 @@ const toPortfolio = () => {
             console.error('Error loading the text file:', error);
         });
 };
-toPortfolio();
 portfolioBtn.addEventListener("click", toPortfolio);
 
